d2c743403b02157355ff8a56ec8dd7877989dc21
Duke Lee <minhduc8a2.1@gmail.com>
2024-01-05 09:57:08 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors())
 app.use(express.json())
-
+app.use("/api/contacts", contactsRouter);
 app.get('/', (req, res) => {
    res.json({msg:'Welcome to contact book application.'})
 })
