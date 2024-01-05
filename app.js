6a8c4def7fef3b7f08be18bbe376cea281f375b8
Duke Lee <minhduc8a2.1@gmail.com>
2024-01-05 10:05:44 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express")
+const cors = require("cors")
 const app = express()
 
-const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route")
+const ApiError = require("./app/api-error")
 app.use(cors())
 app.use(express.json())
-app.use("/api/contacts", contactsRouter);
-app.get('/', (req, res) => {
-   res.json({msg:'Welcome to contact book application.'})
+app.use("/api/contacts", contactsRouter)
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"))
 })
 
-module.exports = app;
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  })
+})
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to contact book application." })
+})
+
+module.exports = app
