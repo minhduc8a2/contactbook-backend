2168def8dc1a1c9259a1f29510569cc2d233739e
Duke Lee <minhduc8a2.1@gmail.com>
2024-01-05 09:45:05 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.json({msg:'Welcome to contact book application.'})
+})
+
+module.exports = app;
